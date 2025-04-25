6a59fda46e9611e766156c08d6e6a94f86ff2bc3
NIMA MD 1V ❤️🙃 <nimsaranimsara704@gmail.com>
2025-04-25 05:42:33 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -1,3 +1,6 @@
+//COD BY LOKU NIMA NIMA MD 1V 2025//
+//MY CONTACT NO 94769091078,94760743488//
+
 const config = require('../config');
 const { cmd } = require('../command');
 const DY_SCRAP = require('@dark-yasiya/scrap');
