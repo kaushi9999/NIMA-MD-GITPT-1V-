a613eda2a0f493b0bb7ad8be8b8db6f759c53df8
NIMA MD 1V ❤️🙃 <nimsaranimsara704@gmail.com>
2025-04-25 21:46:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "true",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/8r95u5.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/tams7i.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
