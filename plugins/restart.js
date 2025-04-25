6d1b1a37768dc54e08ac3dce66cded033b2d3b92
NIMA MD 1V ❤️🙃 <nimsaranimsara704@gmail.com>
2025-04-26 02:00:09 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,9 +1,14 @@
+//COD BY LOKU NIMA NIMA MD 1V 2025//
+//MY CONTACT NO 94769091078,94760743488//
+
+
+
 const { cmd } = require("../command");  
 const { sleep } = require("../lib/functions");  
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart KHAN-MD",  
+    desc: "Restart NIMA-MD",  
     category: "owner",  
     filename: __filename  
 },  
