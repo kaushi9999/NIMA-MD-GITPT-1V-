11061ef32c621622c1ac08bf2db8ea9abece3468
NIMA MD 1V ❤️🙃 <nimsaranimsara704@gmail.com>
2025-05-12 11:18:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NIMA-MD&ZOkFWJKY#DRQ9VmFoFh0_CxUUqONBOGUtnKs8qW9QwmvuM79qAzI",
+SESSION_ID: process.env.SESSION_ID || "NIMA-MD&weFjTACA#r8lIUrdTvhR7xvEWemJo7UfbzeFu_e3XMLvsw0HexPo",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
