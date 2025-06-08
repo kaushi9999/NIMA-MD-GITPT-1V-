78487dc97d6e64280e7750a3d1e5f6b4dc001d17
NIMA MD 1V ❤️🙃 <nimsaranimsara704@gmail.com>
2025-06-08 22:44:57 +0530

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -76,7 +76,7 @@ cmd({
             try {
                 await new Promise(resolve => setTimeout(resolve, 1000)); // Small delay after image
                 await conn.sendMessage(from, {
-                    audio: { url: 'https://files.catbox.moe/wtpcbo.mp3' },
+                    audio: { url: 'https://files.catbox.moe/ipnctb.mp3' },
                     mimetype: 'audio/mp4',
                     ptt: true,
                 }, { quoted: mek });
